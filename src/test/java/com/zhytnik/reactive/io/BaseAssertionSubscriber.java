3ba2e35ddb988be297ff7cfd3778c3feeea58e38
Alexey Zhytnik <zhytnik.aliaksei@consultant.turkcell.com.tr>
2018-01-31 17:10:36 +0300

    add FileReader tests

---
+++ src/test/java/com/zhytnik/reactive/io/BaseAssertionSubscriber.java
@@ -0,0 +1,78 @@
+package com.zhytnik.reactive.io;
+
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 31.01.2018
+ */
+public abstract class BaseAssertionSubscriber<T> implements Subscriber<T> {
+
+    private boolean used;
+    private boolean failed;
+    private boolean completed;
+    private boolean subscribed;
+    private boolean illegalUse;
+
+    private Class expectedError;
+
+    @Override
+    public void onSubscribe(Subscription s) {
+        if (!subscribed && !used && !failed && !completed) {
+            subscribed = true;
+        } else {
+            illegalUse = true;
+            throw new IllegalStateException("Unexpected onSubscribe() call");
+        }
+    }
+
+    @Override
+    public void onNext(T item) {
+        if (subscribed && !failed && !completed) {
+            used = true;
+        } else {
+            illegalUse = true;
+            throw new IllegalStateException("Unexpected onNext() call");
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onError(Throwable error) {
+        if (!failed && !completed) {
+            failed = true;
+
+            if (expectedError == null || !expectedError.isAssignableFrom(error.getClass())) {
+                expectedError = null;
+                illegalUse = true;
+                throw new IllegalStateException("Unexpected exception", error);
+            } else {
+                expectedError = null;
+            }
+        } else {
+            illegalUse = true;
+            throw new IllegalStateException("Unexpected onError() call", error);
+        }
+    }
+
+    @Override
+    public void onComplete() {
+        if (subscribed && !failed && !completed) {
+            completed = true;
+        } else {
+            illegalUse = true;
+            throw new IllegalStateException("Unexpected onComplete() call");
+        }
+    }
+
+    public Subscriber<T> asExpected(Class errorClass) {
+        this.expectedError = errorClass;
+        return this;
+    }
+
+    public void validate() {
+        if (illegalUse) throw new RuntimeException("Illegal behaviour was ignored");
+        if (!subscribed && !used && !failed && !completed) throw new IllegalStateException("Subscriber was unused");
+    }
+}
---
+++ src/test/java/com/zhytnik/reactive/io/FileReaderTest.java
@@ -0,0 +1,161 @@
+package com.zhytnik.reactive.io;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Flow;
+import java.util.function.Supplier;
+
+import static java.lang.Long.MAX_VALUE;
+import static java.nio.ByteBuffer.allocate;
+import static java.nio.file.Files.write;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+/**
+ * @author Alexey Zhytnik
+ * @since 31.01.2018
+ */
+public class FileReaderTest {
+
+    @ClassRule
+    public static TemporaryFolder files = new TemporaryFolder();
+
+    File file;
+    FileReader reader;
+    ReadAssertionSubscriber subscriber;
+    List<byte[]> preparedChunks;
+
+    @Before
+    public void setUp() throws Exception {
+        file = files.newFile();
+        reader = new FileReader(file.toPath());
+        subscriber = new ReadAssertionSubscriber();
+        preparedChunks = new ArrayList<>();
+    }
+
+    @Test
+    public void failsOnWrongResources() {
+        file.delete();
+        reader.subscribe(subscriber.asExpected(NoSuchFileException.class));
+    }
+
+    @Test
+    public void failsOnMissedAllocator() {
+        subscriber.allocator = null;
+        reader.subscribe(subscriber.asExpected(IllegalStateException.class));
+    }
+
+    @Test
+    public void failsOnWrongRequests() {
+        subscriber.request = file.length() + 10;
+        reader.subscribe(subscriber.asExpected(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void lazilyUsesMemory() {
+        subscriber.allocator = () -> null;
+        reader.subscribe(subscriber);
+    }
+
+    @Test
+    public void readsRequestedBytes() throws Exception {
+        addDataForRead(chunk4KB(), chunk4KB(), chunk4KB());
+        subscriber.request = 2 * 4096;
+        reader.subscribe(subscriber);
+
+        assertThat(subscriber.chunks).hasSize(2);
+        assertThat(preparedChunks.get(0)).isEqualTo(subscriber.chunks.get(0));
+        assertThat(preparedChunks.get(1)).isEqualTo(subscriber.chunks.get(1));
+    }
+
+    @Test
+    public void doesNotTouchBytesBeforeReadPosition() throws Exception {
+        addDataForRead(chunk4KB(), chunk4KB());
+
+        subscriber
+                .allocator.get()
+                .position(777).mark()
+                .put((byte) 7)
+                .put((byte) 9);
+
+        reader.subscribe(subscriber);
+
+        assertThat(subscriber.allocator.get().reset().position()).isEqualTo(777);
+        assertThat(subscriber.allocator.get().get(777)).isEqualTo((byte) 7);
+        assertThat(subscriber.allocator.get().get(778)).isEqualTo((byte) 9);
+    }
+
+    @Test
+    public void requestWithMaxLongIsWishToReadResourceFully() throws Exception {
+        subscriber.request = MAX_VALUE;
+        addDataForRead(chunk4KB(), chunk4KB(), chunk4KB());
+        reader.subscribe(subscriber);
+
+        assertThat(subscriber.chunks).hasSize(3);
+        assertThat(preparedChunks.get(0)).isEqualTo(subscriber.chunks.get(0));
+        assertThat(preparedChunks.get(1)).isEqualTo(subscriber.chunks.get(1));
+        assertThat(preparedChunks.get(2)).isEqualTo(subscriber.chunks.get(2));
+    }
+
+    @Test
+    public void readEmptyResources() {
+        subscriber.request = MAX_VALUE;
+        reader.subscribe(subscriber);
+    }
+
+    @After
+    public void tearDown() {
+        if (file.exists() && !file.delete()) {
+            fail("Maybe file is blocked by Reader!");
+        }
+        subscriber.validate();
+    }
+
+    byte[] chunk4KB() {
+        final byte[] bytes = new byte[4096];
+        new Random().nextBytes(bytes);
+
+        preparedChunks.add(bytes);
+        return bytes;
+    }
+
+    void addDataForRead(byte[]... chunks) throws Exception {
+        for (byte[] chunk : chunks) {
+            write(file.toPath(), chunk, APPEND);
+        }
+    }
+
+    static class ReadAssertionSubscriber extends BaseAssertionSubscriber<ByteBuffer> {
+
+        long request;
+        Supplier<ByteBuffer> allocator = new LineReader.MemoryAllocator();
+        List<byte[]> chunks = new ArrayList<>();
+
+        @Override
+        public void onSubscribe(Flow.Subscription s) {
+            super.onSubscribe(s);
+
+            ((FileReader.ReadRequest) s).setAllocator(allocator);
+            s.request(request);
+        }
+
+        @Override
+        public void onNext(ByteBuffer b) {
+            super.onNext(b);
+
+            assertThat(b).isNotNull();
+            chunks.add(allocate(b.limit() - b.position()).put(b).array());
+        }
+    }
+}
